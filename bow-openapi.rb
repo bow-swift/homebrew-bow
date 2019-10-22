6e6126abcc94caec7585145781318af26447392d
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-10-22 18:03:05 +0200

    fix templates path installation
    Update bow-openapi.rb
    
    Update bow-openapi.rb
    
    Update bow-openapi.rb
    
    Update bow-openapi.rb
    
    Update bow-openapi.rb
    
    Update bow-openapi.rb
    
    Update bow-openapi.rb

--- bow-openapi.rb
+++ bow-openapi.rb
@@ -2,7 +2,7 @@ class BowOpenapi < Formula
   desc "Functional HTTP client generate from an OpenAPI/Swagger specification"
   homepage "https://github.com/bow-swift/bow-openapi"
   url "https://github.com/bow-swift/bow-openapi/archive/0.1.0.tar.gz"
-  sha256 "4377b009fdc7fcfd1347238aeb7c471272b8f469a469678135fa0d23c5ab2ea8"
+  sha256 "723ee0272ae606f537215f3071508bd1b1d9a4dd70e637b7feb5315f324e72eb"
 
   depends_on macos: :mojave
   depends_on :xcode => "11.0"
